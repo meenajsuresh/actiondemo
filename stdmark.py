1f483c2eb1cfcffc162618f2e26c6d4d649e3ea8
meenajsuresh <146050298+meenajsuresh@users.noreply.github.com>
2024-02-11 17:20:34 +0530

    stdmark.py added

---
+++ stdmark.py
@@ -0,0 +1,19 @@
+# student_mark
+rank1=95
+rank2=90
+rank3=80
+rank4=70
+rank5=60
+total=(rank1+rank2+rank3+rank4+rank5/5)
+if total>=90:
+    print("A grade")
+elif 80<=total>=89:
+    print("B grade")
+elif 70<=total>=79:
+    print("C grade")
+elif 60<=total>=69:
+    print("D grade")
+else:
+    print("fail")
+
+
